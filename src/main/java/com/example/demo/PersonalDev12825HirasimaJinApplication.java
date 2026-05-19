2e1e6ce400cb375e736d74dbf3fcf45203eb40be
student <student@FJ-1025>
2026-05-19 11:50:54 +0900

    初期作成

---
+++ src/main/java/com/example/demo/PersonalDev12825HirasimaJinApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalDev12825HirasimaJinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalDev12825HirasimaJinApplication.class, args);
+	}
+
+}
