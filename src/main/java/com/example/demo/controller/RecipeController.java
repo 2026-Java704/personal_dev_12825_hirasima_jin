5f6c49f71606f84a089103565d4a3dc6b241d73e
student <student@FJ-1025>
2026-05-20 10:31:20 +0900

    レシピ詳細画面表示

--- src/main/java/com/example/demo/controller/RecipeController.java
+++ src/main/java/com/example/demo/controller/RecipeController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.demo.entity.Category;
@@ -25,6 +26,7 @@ public class RecipeController
 		this.categoryRepository = categoryRepository;
 	}
 	
+	//レシピ一覧表示画面
 	@GetMapping("/recipes")
 	public String index(
 			@RequestParam(defaultValue = "") String keyword,
@@ -41,5 +43,18 @@ public class RecipeController
 		model.addAttribute("keyword", keyword);
 		
 		return "recipes";
+	}
+	
+	//レシピ詳細画面表示
+	@GetMapping("/recipes/detail/{id}")
+	public String detail(
+			@PathVariable int id,
+			Model model)
+	{
+		//表示するレシピ
+		Recipe recipe = recipeRepository.findById(id).get();
+		model.addAttribute("recipe", recipe);
+		
+		return "recipeDetail";
 	}
 }
